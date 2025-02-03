4a85993cf3c4ab13a4ba29692534db96cc1ba4da
sami8890 <samigabol12@gmail.com>
2025-02-03 22:26:46 +0500

    code updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/layout/navbar";
@@ -6,12 +6,9 @@ import Footer from "@/components/layout/footer";
 import { CartProvider } from "@/app/context/cart-context";
 import { Toaster } from "@/components/ui/toaster";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Inter } from "next/font/google";
-import { useState, useEffect } from "react"; // Import useState and useEffect
-import { AnimatePresence } from "framer-motion";
-import LoadingPage from "@/app/loading";
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,27 +21,23 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+export const metadata: Metadata = {
+  title: "The Ecomerce-Furniture Website",
+  description: "The Ecomerce-Furniture Website is a  site for a furniture store.",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
-  const [isLoading, setIsLoading] = useState(true);
+}>)
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-// Add the useEffect hook to the RootLayout component to simulate a loading state for 2 seconds
+{
   return (
     <html lang="en">
       <body
-        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 tracking-tight`}
+        className={`${inter.className} ${geistSans.variable} ${geistMono.variable} antialiased  bg-gray-100  tracking-tight`}
       >
-        <AnimatePresence>{isLoading && <LoadingPage />}</AnimatePresence>
         <main className="wrapper">
           <ClerkProvider>
             <CartProvider>
