dd7eca9b342c4d11a598d6f98984fa3e4efd2bc5
sami8890 <samigabol12@gmail.com>
2025-03-31 16:09:44 +0500

    added more sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import BrowseTheImage from "@/components/main/browse-by-cat";
-import Hero from "../components/main/hero";
 import Products from "@/components/main/our-products";
 import Rooms from "@/components/main/beautifull-romms";
 import FeaturesSection from "@/components/ui/why-choose-us";
 import GallerySection from "@/components/main/Gallery-section";
+import HeroSection from "../components/main/hero";
 export default function Home() {
   return (
     <section className="bg-[#fcfcfc]">
-      <Hero />
+      <HeroSection />
       <BrowseTheImage />
       <Products />
       <Rooms />
